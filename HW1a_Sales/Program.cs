143d47cb2e882803aa3139bfa8b58e490226e2fc
Kristen Foster <Kristen.H.Foster-1@ou.edu>
2025-09-12 11:31:14 -0500

    HW 1 Complete

--- HW1a_Sales/Program.cs
+++ HW1a_Sales/Program.cs
@@ -1,6 +1,6 @@
 ﻿// HW1a Sales Total
 
-// Your Name:
+// Your Name: Kristen Foster
 // Did you seek help ? If yes, specify the helper or web link here: 
 
 using System;
@@ -15,6 +15,21 @@ namespace HW1a_Sales
     {
         static void Main(string[] args)
         {
+            string itemtype;
+            double numberofitems, itemprice;
+            const double taxamt = 0.085;
+
+            Console.WriteLine("What item would you like to purchase?");
+            itemtype = Console.ReadLine();
+            Console.WriteLine($"How many {itemtype} would you like to buy?");
+            numberofitems = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"What is the price of {itemtype}?");
+            itemprice = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"\n\tThe subtotal of your purchase is ${numberofitems * itemprice}.");
+            Console.WriteLine($"\tThe sales tax for your purchase is ${(numberofitems * itemprice) * taxamt}.");
+            Console.WriteLine($"\tThe total of your purchase is ${(itemprice*numberofitems) + taxamt*(itemprice*numberofitems)}.");
+          
+
         }
     }
 }
